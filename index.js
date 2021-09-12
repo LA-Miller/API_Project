aaba5e00b64910819b03391c7a9cfb786bba4cd1
LA-Miller <lamiller516@gmail.com>
2021-09-12 16:10:50 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,91 @@
+const baseURL = 'https://pokeapi.co/api/v2/';
+let url;
+
+// BUTTON
+const submitBtn = document.querySelector('.submit');
+const searchForm = document.querySelector('form');
+
+// RESULTS SECTION
+const section = document.querySelector('section');
+
+searchForm.addEventListener('submit', fetchResults);
+
+function fetchResults(e) {
+    console.log(e);
+    e.preventDefault();
+
+    url = `${baseURL}pokemon?limit=100&offset=200`;
+    console.log('URL:', url);
+
+    fetch(url)
+        .then(function (result) {
+            console.log(result)
+            return result.json();
+        })
+        .then(function (json) {
+            console.log(json);
+            displayResults(json);
+        })
+}
+
+function displayResults(json) {
+    console.log('Display Results', json);
+
+    while(section.firstChild) {
+        section.removeChild(section.firstChild);
+    }
+
+    let pokemon = json.results;
+    for (i = 0; i < pokemon.length; i++) {        
+        // console.log(pokemon[i]);
+
+        let article = document.createElement('article');
+        let heading = document.createElement('h2');
+        let img = document.createElement('img');
+        let para = document.createElement('p');
+        let link = document.createElement('a');
+        let clearFix = document.createElement('div');
+
+        let current = pokemon[i];
+        console.log('Current:', current);
+
+        heading.textContent = '';
+       
+        for(let j = 0; j < current.name.length; j++) {
+            let span = document.createElement('span');
+            span.textContent += current.name[j];
+
+            heading.appendChild(span);       
+        }
+        console.log(heading);
+
+        if(current.sprites.length > 0) {
+            img.src = "http://pokeapi.co/media/sprites/pokemon"
+            img.alt = current.heading
+        }
+
+
+
+        for(let i = 0; j < current.name)
+
+        clearFix.setAttribute('class', 'clearFix');
+
+        article.appendChild(heading);
+        article.appendChild(img);
+        article.appendChild(para);
+        article.appendChild(link);
+        article.appendChild(clearFix);
+        section.appendChild(article);  
+
+        
+
+
+    }  
+}
+
+
+
+
+
+
+
